199c2dfc8d7b83f846445347443df55cfcc93203
Michel Dellabeneta <m.dellabeneta@gmail.com>
2022-05-24 13:30:25 -0300

    .

--- index.php
+++ index.php
@@ -3,18 +3,3 @@
     $instance_id = file_get_contents($url);
     echo " <h1> <font color='black'> Instance ID: <b>" . $instance_id . "</b><br/></font></h1>";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body bgcolor="grey">
-    <div>
-       <h1><a href="https://microsoft.com">MICROSOFT MICHEL</a></h1>
-    </div>
-</body>
-</html>

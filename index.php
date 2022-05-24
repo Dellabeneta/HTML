e8ae9769c43fceaca4be6cc5a7f7b772811a181e
Michel Dellabeneta <m.dellabeneta@gmail.com>
2022-05-24 15:55:16 -0300

    .

--- index.php
+++ index.php
@@ -3,3 +3,16 @@
     $instance_id = file_get_contents($url);
     echo " <h1> <font color='black'> Instance ID: <b>" . $instance_id . "</b><br/></font></h1>";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dellabeneta</title>
+</head>
+<body>
+    <h1>Michel Torres Dellabeneta Page Test</h1>
+</body>
+</html>
